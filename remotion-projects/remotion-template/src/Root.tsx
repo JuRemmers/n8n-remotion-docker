346729ea52cb79d5a91f2926623b08e1b15168f9
JuRemmers <jul.remmers@gmail.com>
2025-06-23 20:46:03 +0200

    Update Root.tsx

--- remotion-projects/remotion-template/src/Root.tsx
+++ remotion-projects/remotion-template/src/Root.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import { staticFile } from 'remotion';
-import { Composition } from 'remotion';
-import { PagesComp, DataSchema } from './Pages';
-import { MyComposition } from "./Composition";
-import { CompSchema } from "./Composition";
+import { Composition, staticFile } from 'remotion';
 import { MyComposition as NewsComposition } from './News';
 import type { NewsSceneProps } from './News'; 
 
@@ -24,30 +20,16 @@ const NEWS_DURATION = demoScenes.length * 10 * 30;
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      {/* News bumper composition */}
       <Composition
-        id="MyComp"
-        component={MyComposition}
-	schema={CompSchema}
-	defaultProps={{
-		headline: 'This is the headline',
-		fontScale: 80,
-		imgUrl: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=1280&q=80",
-		}}
-        durationInFrames={240}
+        id="News"
+        component={NewsComposition}
+        defaultProps={{ scenes: demoScenes }}
+        durationInFrames={NEWS_DURATION}
         fps={30}
         width={1080}
         height={1920}
       />
-	    
-{/* News bumper composition */}
-    <Composition
-      id="NewsComp"                 // will show up as “NewsComp” in Remotion Studio
-      component={NewsComposition}   // points to MyComposition inside News.tsx
-      defaultProps={{ scenes: demoScenes }}
-      durationInFrames={NEWS_DURATION}
-      fps={30}
-      width={1080}
-      height={1920}
-    />
-  </>
-);
+    </>
+  );
+};
