eec3130ddfbea9e8bec5bc83ffe8813bedb07edd
JuRemmers <jul.remmers@gmail.com>
2025-07-03 19:32:53 +0200

    Delete remotion-projects/remotion-template/src/Update.sh

--- remotion-projects/remotion-template/src/Update.sh
+++
@@ -1,3 +0,0 @@
-docker cp E:\DockerFiles\RemotionDocker\remotion-projects\remotion-template\src\Composition.tsx remotion-n8n-container:/app/remotion-projects/remotion-template/src/Composition.tsx
-
-docker cp E:\DockerFiles\RemotionDocker\remotion-projects\remotion-template\src\Root.tsx remotion-n8n-container:/app/remotion-projects/remotion-template/src/Root.tsx
