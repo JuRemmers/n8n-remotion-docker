d9367536c4a3f28e120778c153a6d2eb63894156
JuRemmers <jul.remmers@gmail.com>
2025-07-10 17:38:14 +0200

    Added timing for the server

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-
 echo "Starting n8n..."
 echo "n8n will be available on port 5678"
 echo ""
@@ -10,6 +9,19 @@ echo "  docker exec -it <container-name> sh"
 echo "  cd /app/remotion-projects/my-template"
 echo "  npm start"
 echo ""
+echo "Service will run for 8 hours then shut down"
+echo ""
 
-# Start n8n in foreground (this keeps the container alive)
-exec n8n start
+# Start n8n in background
+n8n start &
+N8N_PID=$!
+
+# Wait for 8 hours (28800 seconds)
+sleep 28800
+
+# Gracefully shutdown n8n
+echo "8 hours elapsed, shutting down n8n..."
+kill -TERM $N8N_PID
+wait $N8N_PID
+
+echo "n8n has been shut down"
